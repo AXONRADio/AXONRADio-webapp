6b489de3d476829521cc5e4df6ea44b5aa561fda
henryq95 <quinteros7766@gmail.com>
2018-03-28 14:44:23 -0700

    refactored main.js and app.vue, main.js now only shows <App/> and previous template is now expored by App.vue

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,25 +1,41 @@
 <template>
-  <div id="app">
-
+    <div id="app">
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <ul>
+        <li><router-link to="/"><a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a></router-link></li>
+        <li><router-link to="/mp3"><a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">MP3 Player</a></router-link></li>
+        <li><a href="http://localhost:4000">Login</a></li>
+        <li><a href="http://localhost:4000/logout">Logout</a></li>
+        <li><a href="http://localhost:4000/profile">My Profile</a></li>
+      </ul>
+    </div>
+    <div class="tab-content" id="v-pills-tabContent">
+      <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">Test</div>
+      <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+      <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+      <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+    </div>
+    <div>
+      <h1>AXONRADiO</h1>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
-
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: red;
   margin-top: 60px;
 }
 </style>
--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -6,8 +6,6 @@ import vueResource from 'vue-resource'
 import VueYoutubeEmbed from 'vue-youtube-embed'
 import App from './App'
 
-import Users from './components/Users'
-import Test from './components/Test'
 import SimpleAudio from './components/SimpleAudio'
 import youtubeembed from './components/YoutubeEmbedded.vue'
 
@@ -28,28 +26,10 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  el: '#app',
+  components: {
+    Test, App
+  },
   router,
-  template: `
-  <div id="app">
-    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-      <ul>
-        <li><router-link to="/"><a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a></router-link></li>
-        <li><router-link to="/mp3"><a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">MP3 Player</a></router-link></li>
-        <li><a href="http://localhost:4000">Login</a></li>
-        <li><a href="http://localhost:4000/logout">Logout</a></li>
-        <li><a href="http://localhost:4000/profile">My Profile</a></li>
-      </ul>
-    </div>
-    <div class="tab-content" id="v-pills-tabContent">
-      <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-      <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-      <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-      <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-    </div>
-    <div>
-      <h1>AXONRADiO</h1>
-    </div>
-    <router-view></router-view>
-  </div>
-  `
+  template:'<App></App>'
 }).$mount("#app");
