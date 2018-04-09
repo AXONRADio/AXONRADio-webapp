9a56933f2cbead0b376019fdd1ac1a9b5d3a8b1b
Raymond Nazaryan <rayn2427@gmail.com>
2018-04-09 13:27:49 -0700

    Removed pill navs

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,22 +1,14 @@
 <template>
     <div id="app">
-    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-      <ul>
-        <li><router-link to="/"><a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a></router-link></li>
-        <li><router-link to="/mp3"><a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">MP3 Player</a></router-link></li>
-        <li><a href="http://localhost:4000">Login</a></li>
-        <li><a href="http://localhost:4000/logout">Logout</a></li>
-        <li><a href="http://localhost:4000/profile">My Profile</a></li>
-      </ul>
-    </div>
-    <div class="tab-content" id="v-pills-tabContent">
-      <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">Test</div>
-      <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-      <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-      <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-    </div>
-    <div>
       <h1>AXONRADiO</h1>
+      <nav class="nav flex-column">
+        <router-link to="/"><a class="nav-link active" href="#">Home</a></router-link>
+        <router-link to="/mp3"><a class="nav-link" href="#">MP3 Player</a></router-link>
+        <a class="nav-link" href="http://localhost:4000" >Login</a>
+        <a class="nav-link" href="http://localhost:4000/logout">Logout</a>
+        <a class="nav-link" href="http://localhost:4000/profile">My Profile</a>
+      </nav>
+    <div>
     </div>
     <router-view></router-view>
   </div>
@@ -39,5 +31,9 @@ export default {
   text-align: center;
   color: red;
   margin-top: 60px;
+}
+
+nav {
+  text-align: left;
 }
 </style>
